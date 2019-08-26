fb16fdbc7a43ffa364a14b458c1dfbf004d9f293
Huan Zhu <tellzhu@outlook.com>
2019-08-26 09:29:26 +0800

    优化： 1、删除不必要的可靠网络协议，由WCF代替

--- General Lib/src/net/ReliableProtocol.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System.Net.Sockets
-
-Namespace net
-    Public Interface IReliableProtocol
-        Function Request(ByRef sock As Socket, stateinfo As Object) As Object
-        Sub Response(ByRef sock As Socket)
-    End Interface
-End Namespace
