f4ca549d97e709aade44db0497656a0ac8711908
Huan Zhu <tellzhu@outlook.com>
2019-09-05 11:31:51 +0800

    优化： 1、删除操纵DataTable不必要的IndexOfColumnName函数

--- General Lib/src/db/DBExpert.vb
+++ General Lib/src/db/DBExpert.vb
@@ -2,15 +2,6 @@
 
 Namespace db
     Public Class DBExpert
-        Public Shared Function IndexOfColumnName(ByRef dt As DataTable, Name As String) As Integer
-            Dim cnt As Integer = dt.Columns.Count - 1
-            For i As Integer = 0 To cnt
-                If dt.Columns(i).Caption = Name Then
-                    Return i
-                End If
-            Next
-            Return -1
-        End Function
 
         Friend Shared Function Split(ByRef dt As DataTable, ColumnIndex As Integer) As Dictionary(Of String, DataTable)
             Dim rowCount As Integer = dt.Rows.Count - 1
@@ -27,9 +18,6 @@ Namespace db
                     dict.Add(s, subDt)
                 End If
             Next
-            subDt = Nothing
-            rowCount = Nothing
-            s = Nothing
             Return dict
         End Function
 
